2e7262bb276d4719409893b5860f8758aae16e33
Syofyan Zuhad <mail@syofyanzuhad.dev>
2025-12-06 19:36:44 +0700

    chore: Enhance win message in GameBoard component by adding a line break for improved readability and user experience.

--- src/components/GameBoard.vue
+++ src/components/GameBoard.vue
@@ -115,6 +115,7 @@
     <p v-if="lastResult === 'win'">
       Kamu menang!<br>
       <span class="text-sm text-zinc-500">Kata: <span class="font-mono font-bold">{{ targetWord.toUpperCase() }}</span></span>
+      <br>
       <a :href="`/kbbi/${targetWord}`" class="text-blue-500 hover:text-blue-400 transition duration-200">Cek artinya di KBBI</a>
     </p>
     <p v-else>
