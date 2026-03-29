3fe941c7568bc2e9ab317ec2452daf81a6aff6a8
Syofyan Zuhad <mail@syofyanzuhad.dev>
2026-03-29 16:30:43 +0700

    ux: enhance KBBI icon visibility on mobile with vibrant colors

--- src/components/GameBoard.vue
+++ src/components/GameBoard.vue
@@ -187,15 +187,15 @@
         </div>
 
         <!-- KBBI Lookup Link for guessed rows -->
-        <div class="w-8 flex justify-center">
+        <div class="w-10 flex justify-center">
           <a
             v-if="rowIndex < guesses.length && currentLanguage === 'id'"
             :href="`https://kbbi.kemendikdasmen.go.id/entri/${guesses[rowIndex].join('')}`"
             target="_blank"
-            class="p-1.5 text-zinc-600 hover:text-blue-500 hover:bg-blue-500/10 rounded-lg transition-all opacity-100 sm:opacity-0 group-hover/row:opacity-100 focus:opacity-100"
+            class="p-2 text-blue-500 bg-blue-500/10 sm:bg-transparent sm:text-zinc-600 hover:text-blue-500 hover:bg-blue-500/10 rounded-xl transition-all opacity-100 sm:opacity-0 group-hover/row:opacity-100 focus:opacity-100 border border-blue-500/20 sm:border-transparent"
             :title="`Lihat arti kata '${guesses[rowIndex].join('').toUpperCase()}' di KBBI`"
           >
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2.5" stroke="currentColor" class="w-4 h-4">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2.5" stroke="currentColor" class="w-5 h-5">
               <path stroke-linecap="round" stroke-linejoin="round" d="M12 6.042A8.967 8.967 0 006 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.967 0 016 18c1.097 0 2.16.22 3.136.617a5.995 5.995 0 016.728 0 12.17 12.17 0 013.136-.617c1.052 0 2.062.18 3 .512V4.262a8.967 8.967 0 00-3-.512 8.967 8.967 0 00-6 2.292m0-2.292a12.17 12.17 0 013.136-.617c1.052 0 2.062.18 3 .512v14.25A8.987 8.967 0 0118 18a12.17 12.17 0 01-3.136.617m-6.864-14.713a12.115 12.115 0 016.864 0M9 17.01V4.5m1.875 13.5a11.992 11.992 0 003.75 0" />
             </svg>
           </a>
